a023255481b1cf0bf96b606d103d196f7212cb44
fuentebermudez <fuentebermudez@gmail.com>
2015-03-19 23:09:32 +0100

    First attempt to create makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,25 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric(),nrow=1,ncol=1) {
+        matrixObject<-matrix(x,nrow,ncol)
+        inverse<-NULL
+        
+        set <- function(y=numeric(),nrow=numeric(),ncol=numeric()) {
+                x <<- y
+                nrow<<-nrow
+                ncol<<-ncol
+                matrixObject<<-matrix(y,nrow,ncol)
+                inverse <<- NULL
+        }
+        
+        get<-function() matrixObject
+        setInverse<-function(Pinverse) inverse<-Pinverse
+        getInverse<-function() inverse<-solve(matrixObject)
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
@@ -13,3 +30,4 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
